a3fbf25b370144b9ab55d2fb680fe4d6e50ef4f7
rdbende <rdbende@gmail.com>
2022-08-12 22:18:56 +0200

    Delete old tests

--- tests/test_bind_add_value_isnt_bool.py
+++
@@ -1,6 +0,0 @@
-from base import lint
-
-
-def test_bind_add_equals_plus():
-    code = "widget.bind('<Button-1>', print, add='+')"
-    assert lint(code) == {"1:34 TK304 Value for `add` should be a boolean."}
